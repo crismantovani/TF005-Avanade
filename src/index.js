05464de0de4ef276bcc6b30edb6ddae3b4be5726
cbalieiro <bmoliveira.camila@gmail.com>
2021-04-12 15:54:25 -0300

    feature/foldes and routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Root from './components/Root';
+// import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Root />, document.getElementById('root'));
---
+++ src/utils/routes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Routes = () => (
+  <>
+    <h1>Rotas</h1>;
+  </>
+);
+
+export default Routes;
