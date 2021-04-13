ab61746e4e61b6e130aba4dfef9b4e2351b4d582
Kauana Maria Agostini <kauanaagostini@gmail.com>
2021-04-13 00:40:13 -0300

    implementado label botão

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import FaceCapture from '../../components/FaceCapture';
 
 const Home = () => (
   <>
-    <FaceCapture />
     <h1>Home</h1>
   </>
 );
--- src/pages/registerUser/index.js
+++ src/pages/registerUser/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import FaceCapture from '../../components/FaceCapture';
 
 const RegisterUser = () => (
   <>
+
+    <FaceCapture
+      label="Registrar"
+    />
     <h1>RegisterUser</h1>;
   </>
 );
