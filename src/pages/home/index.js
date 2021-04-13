ae576f2f7a208970b2bdedbba39d0ee0083aed8f
crismantovani <crismantovani87@gmail.com>
2021-04-13 01:30:51 -0300

    Cria css global

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import '../../global.css';
+import Button from '../../components/Button';
 import FaceCapture from '../../components/FaceCapture';
 
 const Home = () => (
   <>
     <FaceCapture />
     <h1>Home</h1>
+
+    <Button
+      buttonType='submit'
+      buttonClass='btn-base btn-register'
+      buttonText='registrar'
+    />
   </>
 );
 
