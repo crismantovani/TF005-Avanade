7030541781d811757b8ed405b67af66a548c1b63
Dunia <dunia07@hotmail.com>
2021-04-13 13:23:50 -0300

    fix(images): resizing and background removal

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,22 @@
+/* eslint-disable space-infix-ops */
+/* eslint-disable no-undef */
 import React from 'react';
 import Button from '../Button';
 import './style.css';
+import AvanadeLogoName from '../../images/avanade-logo-name.png';
 
 const Header = () => (
-    <header className="Header">
+    <header className="header">
     <Button
       buttonType="submit"
       buttonClass="button-base"
       buttonText="BACK-Temporario"
+    />
+    <img
+      src={AvanadeLogoName}
+      id='avanade-logo-name'
+      className='avanade-logo-name'
+      alt='avanade-logo-name'
     />
     </header>
 );
