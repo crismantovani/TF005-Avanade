3565306c52567f0bd189dc089ba1339880c79f41
cbalieiro <bmoliveira.camila@gmail.com>
2021-04-13 23:00:42 -0300

    1. removendo arquivos antigos do react. 2.rota para botão pegar com codigo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/Root/Root';
-// import './index.css';
+import './global.css';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
