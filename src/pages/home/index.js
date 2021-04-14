854d8109c7c9b4a6f34899d2fa65adc2c06e6ad4
crismantovani <crismantovani87@gmail.com>
2021-04-13 23:15:08 -0300

    Adiciona estilização de links, span, fonte e cor padrão e deixa o html semântico

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,31 +6,37 @@ import logo from '../../images/avanade-logo.png';
 const Home = () => {
   const history = useHistory();
 
-  return (<>
-    <div className="home-container">
-      <img src={logo} alt='logo' />
-      <main>
-        <h2>O que você gostaria de fazer?</h2>
-        <div className='container-btn-home'>
-          <Button
-            buttonType='button'
-            buttonClass='btn-base btn-register'
-            buttonText='Entregar encomenda'
-            buttonOnClick={() => { history.push('/order/register'); }}
-          />
-          <Button
-            buttonType='button'
-            buttonClass='btn-base btn-register'
-            buttonText='Pegar encomenda'
-            buttonOnClick={() => { history.push('/order/pick'); }}
-          />
+  return (
+    <>
+      <header></header>
+      <div className='main-container-base'>
+        <div className='main-logo'>
+          <figure>
+            <img src={logo} className='main-logo' alt='Logo Avanade' />
+          </figure>
         </div>
-        <p>
-          Não tem cadastro? <span> <Link to='/user/register'>Registrar-se</Link></span>
-        </p>
-      </main>
-    </div>
-  </>
+        <main>
+          <h2>O que você gostaria de fazer?</h2>
+          <div className='container-btn-home'>
+            <Button
+              buttonType='button'
+              buttonClass='btn-base btn-register btn-centered'
+              buttonText='Entregar uma encomenda'
+              buttonOnClick={() => { history.push('/order/register'); }}
+            />
+            <Button
+              buttonType='button'
+              buttonClass='btn-base btn-register btn-centered'
+              buttonText='Pegar encomenda'
+              buttonOnClick={() => { history.push('/order/pick'); }}
+            />
+          </div>
+          <p>
+            Não tem cadastro? <span> <Link to='/user/register'>Registrar-se</Link></span>
+          </p>
+        </main>
+      </div>
+    </>
   );
 };
 
