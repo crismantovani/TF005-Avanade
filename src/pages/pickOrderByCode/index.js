9b6c7848e3a5e661aa1f3807051689b621060111
cbalieiro <bmoliveira.camila@gmail.com>
2021-04-14 08:58:22 -0300

    criado e configurado arquivo para consumir mockapi

--- src/pages/pickOrderByCode/index.js
+++ src/pages/pickOrderByCode/index.js
@@ -1,21 +1,30 @@
-import React from 'react';
+/* eslint-disable comma-dangle */
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Button from '../../components/Button';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
-import Modal from '../../components/Modal/Modal';
+// import Modal from '../../components/Modal/Modal';
+import Input from '../../components/Input/Input';
+import LockerFace from '../../utils/LockerFaceAPIconfig';
 
 const PickOrderByCode = () => {
   const history = useHistory();
+  const [tracking, setTracking] = useState();
 
   return (<>
     <Header />
-    <Modal
-      modalType='error'
-      modalText='errouuu'
-    />
     <main>
-      <h2>PÁGINA NOVA - Pegar pelo Código</h2>
+      <h2>Localizar encomenda pelo Código</h2>
+      <Input
+        type='number'
+        className='form-inputs'
+        onChange={(e) => setTracking(e.target.value)}
+        placeholder='Código de entrega'
+        minLength='3'
+        maxLength='6'
+        required
+      />
       <Button
         buttonType='button'
         buttonClass='btn-base btn-register'
